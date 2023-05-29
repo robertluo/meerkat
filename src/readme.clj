0a0cee4aaad6aa918e1170a000fbf8c6e8310dae
Luo Tian <lt@basecity.com>
2023-05-28 19:29:55 -0700

    readme

---
+++ src/readme.clj
@@ -0,0 +1,15 @@
+(ns readme
+  "Readme for meerkat")
+
+;; ## Background
+;;
+;; Large language models (LLMs) are cool, and as a Clojure programmer, I want to try them. However, most libraries are in Python. 
+;;
+;; I could switch to Python to take advantage of the ecosystem, but I really miss Clojure's functional style.
+;;
+;; Meerkat is my attempt to understand and practice (supporting OpenAI only for now).
+;;
+;; ## Unlicense
+;;
+;; Robert Luo, 2023.
+
